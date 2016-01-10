0b27c4e8bd73d4bf6c8931a7865ed8746514a04c
Teddy Rilliot <rilliot.teddy@outlook.com>
2016-01-10 01:20:59 +0100

    Added examples for random and sequential lists

---
+++ examples/random/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/TeddyRilliot/list/random"
+)
+
+type item struct {
+	ID   int
+	Name string
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var items = []item{
+	item{1, "Title 1"},
+	item{2, "Title 2"},
+	item{3, "Title 3"},
+}
+
+func main() {
+	seq := random.New(items)
+
+	for l := 0; l < 3; l++ {
+		fmt.Println("Loop ", l)
+		for i := 1; i <= len(items); i++ {
+			fmt.Printf("\t%v\n", seq.Next())
+		}
+	}
+}
